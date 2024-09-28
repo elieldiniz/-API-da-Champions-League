d95b09a6954fd2cf75c0547148d5d038f1bc7230
elieldiniz <elieldiniz1@outlook.com>
2024-09-28 00:34:26 -0300

    adding module app(json.())

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import express from "express"
+import express, { json } from "express"
 
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT 
+
+app.use(json())
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the Express API!' })
 })
 
 app.listen(PORT,()=>
-    console.log('Server is running on port 3000')
+    console.log(`Server is running on port${PORT}`)
 ) 
