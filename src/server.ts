005b338050c439ce94415a66ac88866747fb1b54
elieldiniz <elieldiniz1@outlook.com>
2024-09-28 00:27:23 -0300

    adding expressjs port 3000

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
+import express from "express"
 
 
-console.log("Loading")
+const app = express()
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Express API!' })
+})
+
+app.listen(PORT,()=>
+    console.log('Server is running on port 3000')
+) 
