86979861b004879ad2fb9d5191450168f929d525
elieldiniz <elieldiniz1@outlook.com>
2024-10-02 22:08:39 -0300

    adding Readmin

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import Express from "express"
 import router from "./routers";
+import cors from "cors";
 
 function createApp(){
     const app = Express();
 
     app.use(Express.json());
 
-    app.use("/api", router) 
+    app.use("/api", router)
+
+    //UseCors
+    
+    //const corsOptions = {
+        //origin: ["http://localhost"],
+        
+    //}
+    //app.use(cors())
 
     return app;
 }
